41472509ed58256afdc4aa60a6886199a3079235
Denis Borysenko <dborysenko.ed@gmail.com>
2024-02-20 10:35:23 +0200

    major refactor

--- pract-5/zavd 3.py
+++ pract-5/zavd 3.py
@@ -1,36 +1,23 @@
 integer = int(input("input an integer: "))
 
-# def exclude_starting_zeroes(digit): 
-#     digit = str(digit)
-#     while "0" in digit[0]:
-#         digit = digit[1:]
-#     return int(digit)
-
-# integer = exclude_starting_zeroes(integer)   
-
+# спочатку потрібно прибрати всі 0 на початку числа: 
 integer = str(integer)
 while "0" in integer[0]:
     integer = integer[1:]
 
+str_int = str(integer) # приведення до рядка числа без 0 на початку
+len_int = len(str_int) # кількістсь символів до рядка
 
-str_int = str(integer)
-#print(str_int)
-#print(integer)
-len_int = len(str_int)
-#print(len_int)
-total = []
-
-dividers = []
-for divider in range(0, len_int):
-    divider = "1" + "0" * divider
-    dividers.append(divider)
-dividers = dividers[::-1]
-#print(dividers)
+# визначаємо кількість дільників та кожен окремий дільник
+dividers = [] 
+for divider in range(0, len_int): 
+    divider = "1" + "0" * divider # визнначаємо "розряд" кожного дільника
+    dividers.append(divider) # записуємо в масив всі дільники (автоматично записується від 1 до найбільшого )
+dividers = dividers[::-1] # змінюємо порядок дільникв
 
 result = []
 for i in range(0, len_int): 
     result.append(str_int[i] + "*" + dividers[i])
-    
 
 print(str_int+" = "+"+".join(result))
 

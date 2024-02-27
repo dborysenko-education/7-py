f7c9c348c50405efee6c1caa32f8e07d3a5595b1
dborysenko-education <158144849+dborysenko-education@users.noreply.github.com>
2024-02-28 01:19:49 +0200

    eliminated zeroes-only input

--- zavd 3-complete.py
+++ zavd 3-complete.py
@@ -1,6 +1,5 @@
 integer = input("input an integer: ")
 
-
 # визначаємо позитивне чи негативне число ввів користувач
 negative = False
 if integer[0] == "-":
@@ -9,16 +8,25 @@ if integer[0] == "-":
 
 # відфільтровуємо все, що не є виключно цілочисленними значеннями
 if integer.isdigit() == False:
-    print("Only numbers are accepted!")
-    exit()
-
-
+    exit("Only numbers are accepted!")
 
 # прибраємо всі 0 на початку числа: 
+zeroes = 0
+for i in range(0, len(integer)): 
+    if integer[i] == "0": 
+        zeroes += 1
+
+if len(integer) == zeroes: 
+    exit("you entered only zeroes")
+
 while "0" in integer[0]:
     integer = integer[1:]
 
-len_int = len(integer) # кількістсь символів до рядка
+len_int = len(integer) # кількістсь символів 
+
+if len_int != 4: 
+    exit(f"Only 4-digit integers accepted, your integer has {len_int} digits!")
+
 chars_number = range(0, len_int)
 
 # визначаємо кількість дільників та кожен окремий дільник
@@ -26,7 +34,6 @@ dividers = []
 for divider in chars_number: 
     divider = "1" + "0" * divider # визнначаємо "розряд" кожного дільника
     dividers.insert(0, divider) # записуємо в масив всі дільники (автоматично записується від 1 до найбільшого )
-
 
 results = []
 for i in chars_number: 

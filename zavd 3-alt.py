725c62b4f578e46b30dd6e2f4bb88125685b4bae
dborysenko-education <158144849+dborysenko-education@users.noreply.github.com>
2024-02-27 08:51:13 +0200

    Update zavd 3-alt.py
    refactor

--- zavd 3-alt.py
+++ zavd 3-alt.py
@@ -5,16 +5,16 @@ while "0" in integer[0]:
     integer = integer[1:]
 
 len_int = len(integer) # кількістсь символів у числі
-
+int_chars = range(0, len_int)
 # визначаємо кількість дільників та кожен окремий дільник
 dividers = [] 
-for divider in range(0, len_int): 
+for divider in int_chars: 
     divider = "1" + "0" * divider # визнначаємо "розряд" кожного дільника
     dividers.append(divider) # записуємо в масив всі дільники (автоматично записується від 1 до найбільшого )
 dividers = dividers[::-1] # змінюємо порядок дільникв
 
 result = []
-for i in range(0, len_int): 
+for i in int_chars: 
     result.append(integer[i] + "*" + dividers[i])
 beautiful_result = '+'.join(result)
 

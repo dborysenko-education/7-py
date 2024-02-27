b783d3dba63480d84ae6d4efcfc8f026260ed899
dborysenko-education <158144849+dborysenko-education@users.noreply.github.com>
2024-02-28 00:39:15 +0200

    complete version

--- zavd 3-complete.py
+++ zavd 3-complete.py
@@ -14,31 +14,15 @@ if integer.isdigit() == False:
 while "0" in integer[0]:
     integer = integer[1:]
 
-<<<<<<< Updated upstream
-len_int = len(integer) # кількістсь символів у числі
-int_chars = range(0, len_int)
-# визначаємо кількість дільників та кожен окремий дільник
-dividers = [] 
-for divider in int_chars: 
-=======
 len_int = len(integer) # кількістсь символів до рядка
 chars_number = range(0, len_int)
 
 # визначаємо кількість дільників та кожен окремий дільник
 dividers = [] 
 for divider in chars_number: 
->>>>>>> Stashed changes
     divider = "1" + "0" * divider # визнначаємо "розряд" кожного дільника
     dividers.insert(0, divider) # записуємо в масив всі дільники (автоматично записується від 1 до найбільшого )
 
-<<<<<<< Updated upstream
-result = []
-for i in int_chars: 
-    result.append(integer[i] + "*" + dividers[i])
-beautiful_result = '+'.join(result)
-
-print(f"{integer} = {beautiful_result}")
-=======
 
 results = []
 for i in chars_number: 
@@ -54,4 +38,3 @@ for i in chars_number:
 beautiful_result = ' + '.join(results)
 
 print(f"{minus}{integer} => {beautiful_result}")
->>>>>>> Stashed changes
